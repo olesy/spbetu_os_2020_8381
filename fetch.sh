657b04f1b2e201aeaa2f793f5ad0eebff46944af
Michael <jakutenshi@gmail.com>
2020-03-11 09:54:50 +0300

    Add new students to scrripts 11.03.19

--- fetch.sh
+++ fetch.sh
@@ -3,14 +3,14 @@
 # git fetch Elli-zveg         # Звегинцева Елизавета
 # git fetch garet2gis         # Муковский Данил
 git fetch the21composer     # Облизанов Александр
-# git fetch olesy             # Ивлева Олеся
+git fetch olesy             # Ивлева Олеся
 # git fetch StavrWalker       # Гоголев Евгений
 # git fetch Fireralin         # Перелыгин Дмитрий
 git fetch Nik-Poch          # Почаев Никита
 # git fetch NguyenHai-LETI    # Нгуен Ши Хай
 # git fetch Dmitriy129        # Переверзев Дмитрий
-# git fetch GandhiKK          # Киреев Константин
+git fetch GandhiKK          # Киреев Константин
 # git fetch milesartemius     # Сергеев Александр
 # git fetch Anstberd          # Бердникова Анастасия
-# git fetch marilisok         # Лисок Мария
+git fetch marilisok         # Лисок Мария
 # git fetch DmitriySosnovskiy # Сосновский Дмитрий
